98f13bb5c1fe9c8469f6dae74d31796dc065367b
Illya <91430675+sigizmunde@users.noreply.github.com>
2022-04-25 02:46:00 +0300

    second example done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+
+generateGalleryMarkup(galleryItems, refs.gallery);
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "self",
+  captionType: "attr",
+  captionAttribute: "title",
+  captionPosition: "bottom",
+  captionDelay: 0,
+  showCounter: false,
+});
+
+function generateGalleryMarkup(items, containerNode) {
+  const images = items.map((item) => {
+    const galleryItem = document.createElement("a");
+    galleryItem.classList.add("gallery__item");
+    galleryItem.href = item.original;
+    galleryItem.title = item.description;
+    galleryItem.innerHTML = `<img class="gallery__image"
+          src="${item.preview}"
+          alt="${item.description}" />`;
+    return galleryItem;
+  });
+  containerNode.append(...images);
+}
+
+// console.log(galleryItems);
